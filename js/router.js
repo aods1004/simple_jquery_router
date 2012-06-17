8ff1f0b3f1da91a512f41fe331e9db767bbc2cfc
amemura <aods1004@yahoo.co.jp>
2012-06-18 02:20:26 +0900

    IE対応

--- js/router.js
+++ js/router.js
@@ -8,9 +8,10 @@
     "use strict";
     var Route,
         Router,
-        params_exp = /(\/:[a-z\-]+)/gi,
-        match_exp  = /[a-z\-]+/i,
-        replace_to_string = '\\/([a-z0-9]+)';
+        _exp_routes_param = /(\/:[a-z\-]+)/gi,
+        _exp_path_param   = /[a-z\-]+/i,
+        _str_routes_replace = '\\/([a-z0-9]+)', 
+        _exp_match_hash    = /#([^#]*)$/;
 
     /**
      * ルートエンティティクラス
@@ -19,12 +20,24 @@
      * 
      */
     Route = function (path, callback) {
+        
+        /**
+         *  @var callback ルーティング後処理を引き渡すコールバック
+         */
         this.callback = callback;
-        this.map = [];
-        this.matches = [];
-        this.regexp = '';
-        this.makeRegexp.call(this, path);
+        /**
+         *  @var map {array} パラメーターと順番のマッピング用配列
+         */
+        this.map        = [];
+        /**
+         *  matches 
+         */
+        this.matches    = [];
+        this.regexp     = '';
+        this.makeRegexp(path);
+        delete this.makeRegexp;
     };
+    
     /**
      * 設定されたルートパスを正規表現にパースする
      * @param {string} path ルート 
@@ -32,11 +45,11 @@
     Route.prototype.makeRegexp = function (path) {
         var i, 
             l, 
-            matches  = path.match(params_exp),
-            replaced = path.replace(params_exp, replace_to_string);
+            matches  = path.match(_exp_routes_param),
+            replaced = path.replace(_exp_routes_param, _str_routes_replace);
         if (matches && matches.length > 0) {
             for (i = 0, l = matches.length; i < l; i += 1) {
-                this.map[i] = matches[i].match(match_exp)[0];
+                this.map[i] = matches[i].match(_exp_path_param)[0];
             }
         }
         this.regexp = new RegExp('^' + replaced + '\\/?$');
@@ -51,9 +64,8 @@
         if (matches && matches.length > 0) {
             this.matches = matches.slice(1);
             return true;
-        } else {
-            return false;
         }
+        return false;
     };
     /**
      *  マッチの配列と、ルータークラスリクエストパラメーターを取得する
@@ -64,16 +76,32 @@
             request[this.map[i]] = this.matches[i];
         }
         return request;
-    }, 
+    };
     /**
      * ルーターオブジェクト
-     * 
      */
     Router = {
+        
         /**
-         *  ルートクラスの配列
+         *  @var {object} settings ルーター設定
+         **/
+        settings: {
+            controllerClass: 'ctrl'
+        }, 
+        
+        /**
+         * 設定する
+         * 
+         */
+        setting: function (args) {
+            this.settings = $.extend(this.settings, args);
+        }, 
+        
+        /**
+         *  @var {array} routes ルートクラスの配列
          */
         routes: [],
+        
         /**
          *  ルートを設定する
          */
@@ -81,19 +109,41 @@
             this.routes.push(new Route(path, callback));
             return this;
         },
+        
         /**
          *  現在のパスを返す
          */
         getPath: function () {
             return window.location.hash.slice(1);
         },
+
+        /**
+         *  現在のパスを返す
+         */
+        
+        currentPath : window.location.hash.slice(1), 
+        
+        getCurrentPath: function () {
+            return this.currentPath;
+        },
+        setCurrentPath: function (str) {
+            this.currentPath = str;
+            return this;
+        }, 
+        
+        /**
+         * 文字列からハッシュ部分を取り出す
+         */
+        getHash: function (str) {
+            return str.match(_exp_match_hash)[1] || '';
+        }, 
+        
         /**
          *  設定されたルートに基づき処理を振り分ける
          */
         dispatch: function () {
             var i, 
                 l, 
-                matches, 
                 path    = this.getPath(),
                 routes  = this.routes;
             for (i = 0, l = routes.length; i < l; i += 1) {
@@ -106,25 +156,40 @@
                 }
             }
         }, 
+        
         /**
          *  windowオブジェクトのhashchangeイベントにルータをセットする
-         *  IE7では動作しない。
+         *  とIE7で動作しないのでonclickでも対応する。
          *  
          *  @return {object} this
          */
         deploy: function () {
-            $(window).bind('hashchange', $.proxy( function () { 
-                this.dispatch();
-            }, Router));
+            var self = this;
+            $(window.document).on(
+                'click', 'a.' + self.settings.controllerClass, 
+                function () { 
+                    var elm  = this;
+                    window.setTimeout(function () {
+                        self.save(self.getHash($(elm).prop('href')), false);
+                    }, 20);
+                }
+            );
+            $(window).on('hashchange', $.proxy(function () {
+                var path = this.getPath();
+                this.save(this.getPath(), false);
+            }, self));
             return this;
         }, 
         /**
          *  パスを変更し、ルーティングを発生させる
          */
-        save: function (str) {
-            if (this.getPath() !== str) {
-                window.location.hash = str;
-            } else {
+        save: function (str, hashchange_flg) {
+            hashchange_flg = hashchange_flg || false;
+            if (this.getCurrentPath() !== str) {
+                if (hashchange_flg) {
+                    window.location.hash = str;
+                }
+                this.setCurrentPath(str);
                 this.dispatch();
             }
         }
